a2e784c4f0849e7ecc42ec6680f145a0fec5a2ce
AnsisKv <ansiskvelde@icloud.com>
2023-05-12 18:25:22 +0300

    Initial commit

---
+++ OpenWeatherApp/Controllers/ChangeCityViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ChangeCityViewController.swift
+//  OpenWeatherApp
+//
+//  Created by kvelde.niklavs.ansis on 28/04/2023.
+//
+
+import UIKit
+
+protocol ChangeCityDelegate {
+    func userEnterCityName(city: String)
+}
+
+class ChangeCityViewController: UIViewController {
+
+    var delegate: ChangeCityDelegate?
+    @IBOutlet weak var cityTextField: DesignableTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func getWeatherTapped(_ sender: Any) {
+        
+        guard let cityName = cityTextField.text else { return }
+        
+        if !cityName.isEmpty {
+            delegate?.userEnterCityName(city: cityName)
+            
+            self.dismiss(animated: true)
+        }
+        
+    }
+    
+
+}
---
+++ OpenWeatherApp/Controllers/WeatherViewController.swift
@@ -0,0 +1,90 @@
+//
+//  WeatherViewController.swift
+//  OpenWeatherApp
+//
+//  Created by kvelde.niklavs.ansis on 28/04/2023.
+//
+
+import UIKit
+import CoreLocation
+import Alamofire
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate, ChangeCityDelegate {
+    
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    var openWeather: OpenWeather?
+    let openWeatherModel = OpenWeatherModel()
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            print("long: \(location.coordinate.longitude), lat: \(location.coordinate.latitude)")
+            
+            let long = String(location.coordinate.longitude)
+            let lat = String(location.coordinate.latitude)
+            
+            let params: [String: String] = ["lat": lat, "lon": long, "appid": openWeatherModel.apiId]
+            getWeatherData(url: openWeatherModel.apiUrl, params: params)
+        }
+    }
+    
+
+    func getWeatherData(url: String, params: [String: String] ){
+        AF.request(url, method: .get, parameters: params).responseData { response in
+            if response.value != nil {
+                do {
+                    let jsonData = try JSONDecoder().decode(OpenWeather.self, from: response.value!)
+                    self.openWeather = jsonData
+                    self.updateWeather(data: self.openWeather!)
+                } catch {
+                    print("err::::", error)
+                }
+            }else{
+                self.cityLabel.text = "Weather Unavailible 😢"
+            }
+        }
+    }//getWeatherData
+    
+    func updateWeather(data: OpenWeather){
+        if let result = data.main?.temp {
+            cityLabel.text = "\(data.name ?? "")" + ", \(data.sys?.country ?? "")"
+            tempLabel.text = "\(Int(result - 273)) º"
+            data.weather?.forEach({ weath in
+                weatherIcon.image = UIImage(named: openWeatherModel.updateWeatherIcon(condition: weath.id ?? 0))
+            })
+        }else{
+            self.cityLabel.text = "Weather Unavailible 😢"
+        }
+    }//updateWeather
+    
+    
+    func userEnterCityName(city: String) {
+        print(city)
+        let params: [String: String] = ["q": city, "appid": openWeatherModel.apiId]
+        
+        getWeatherData(url: openWeatherModel.apiUrl, params: params)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "city" {
+            let vc = segue.destination as! ChangeCityViewController
+            vc.delegate = self
+        }
+    }
+
+}
+
